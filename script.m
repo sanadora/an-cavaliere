0835c2ab49ae95a40cb63d7d24d26163d38a7005
Pedro xubuntu <ppmauro@hotmail.com>
2015-04-24 00:15:44 -0300

    Primeros dos puntos realizados

--- script.m
+++ script.m
@@ -1,18 +1,21 @@
 tabla_b = calcular_tabla(5, 0.0001);
 tabla_b_refinada = refinar(5, 0.0001, obtener_w(tabla_b));
 save tablas_b.mat tabla_b tabla_b_refinada
+disp("Punto b realizado")
 
 tabla_c1 = calcular_tabla(5, 0.00001);
 tabla_c1_refinada = refinar(5, 0.00001, obtener_w(tabla_c1));
 tabla_c2 = calcular_tabla(5, 0.001);
 tabla_c2_refinada = refinar(5, 0.001, obtener_w(tabla_c2));
 save tablas_c.mat tabla_c1 tabla_c1_refinada tabla_c2 tabla_c2_refinada
+disp("Punto c realizado")
 
 tabla_d = calcular_tabla(6, 0.0001);
 tabla_d_refinada = refinar(6, 0.0001, obtener_w(tabla_d));
 save tablas_d.mat tabla_d tabla_d_refinada
+disp("Punto d realizado")
 
 tabla_e = calcular2(5, 0.0001);
 tabla_e_refinada = refinar(5, 0.0001, obtener_w(tabla_e));
 save tablas_e.mat tabla_e tabla_e_refinada
-
+disp("FFFFIIIINNNN!!")
