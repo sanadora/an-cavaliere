5ebf1adc21f66d56e827dcae3c0b02d21d504153
Pedro xubuntu papa <ppmauro@hotmail.com>
2015-04-27 07:42:50 -0300

    Tablas de puntos b, c y d realizadas

--- script.m
+++ script.m
@@ -4,6 +4,8 @@
 b = tabla_sor(5, 0.0001);
 b_refinada = refinar_tabla_sor(5, 0.0001, mejor_w(b));
 save tablas_b.mat b b_refinada
+graficar(b, "Omega optimo para n=5 y RTOL=0.001");
+print b.png;
 graficar(b_refinada, "Omega optimo para n=5 y RTOL=0.001");
 print b_refinada.png;
 disp("Punto b realizado")
@@ -18,8 +20,12 @@ c2 = tabla_sor(5, 0.001);
 c2_refinada = refinar_tabla_sor(5, 0.001, mejor_w(c2));
 save tablas_c.mat c1 c1_refinada c2 c2_refinada
 
+graficar(c1, "Omega optimo para n=5 y RTOL=0.00001");
+print c1.png;
 graficar(c1_refinada, "Omega optimo para n=5 y RTOL=0.00001");
 print c1_refinada.png;
+graficar(c2, "Omega optimo para n=5 y RTOL=0.001");
+print c2.png;
 graficar(c2_refinada, "Omega optimo para n=5 y RTOL=0.001");
 print c2_refinada.png;
 disp("Punto c realizado")
@@ -31,6 +37,8 @@ disp("Punto c realizado")
 d = tabla_sor(6, 0.0001);
 d_refinada = refinar_tabla_sor(6, 0.0001, mejor_w(d));
 save tablas_d.mat d d_refinada
+graficar(d, "Omega optimo para n=6 y RTOL=0.0001");
+print d.png;
 graficar(d_refinada, "Omega optimo para n=6 y RTOL=0.0001");
 print d_refinada.png;
 disp("Punto d realizado")
@@ -42,5 +50,7 @@ disp("Punto d realizado")
 e = tabla_sor_corte_x_previo(5, 0.0001);
 e_refinada = refinar_tabla_sor_corte_x_previo(5, 0.0001, mejor_w(e));
 save tablas_e.mat e e_refinada
+graficar(e, "Omega optimo para n=5 y RTOL=0.0001 con criterio de corte: X anterior");
+print e.png;
 graficar(e_refinada, "Omega optimo para n=5 y RTOL=0.0001 con criterio de corte: X anterior");
 print e_refinada.png;
